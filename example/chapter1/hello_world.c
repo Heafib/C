2b5ebdffc4abcf9d01a2624cc808b3e20f8ee1ea
Heafib <heafib@gmail.com>
2016-05-19 20:19:13 +0000

    add chapetr1

---
+++ example/chapter1/hello_world.c
@@ -0,0 +1,24 @@
+/*
+* Copyright (c) 2016 Heafib
+* 文件名：Hello_world.c
+* 描述：运行程序后在终端上输出指定的内容
+* 作者：Heafib
+* 完成日期：2016年5月19日
+* 当前版本：1.0
+*/
+#include <stdio.h>
+/*
+* 函数名称：main
+* 描述：程序的入口
+* 作者：Heafib
+* 完成日期：2016年5月19日
+* 输入：void（不接受任何参数）
+* 返回：整形
+* 当前版本：1.0
+*/
+int main(void)
+{   //在终端上输出Hello World
+    /* 输出Hello World */
+    printf("Hello world\n");
+    return 0; 
+}
